159dd5531c376a9620809d5164a0155ee328b5cc
unknown <oldwhpsian@gmail.com>
2023-02-20 22:31:13 +0000

    Added docstrings to the ManualAnalysis class

--- investment_tools/fmp/fmp_layer_1.py
+++ investment_tools/fmp/fmp_layer_1.py
@@ -423,31 +423,55 @@ class FinancialData:
 
 
 class ManualAnalysis:
-    """Financial Data Analysis class
+    """
+    Class for performing manual financial analysis.
 
-    Analyzes financial data and returns important metrics and ratios.
-    
-    Parameters
-    ----------
-    financial_data : object
-        Financial data to be analyzed
-    
-    Attributes
-    ----------
-    data : object
-        Financial data
-    calculated_metrics : pd.DataFrame
-        Metrics calculated from the financial data
-    reported_metrics : pd.DataFrame
-        Metrics reported in the financial data
-    metric_errors : pd.DataFrame
-        Error between calculated and reported metrics
-    ratio_errors : pd.DataFrame
-        Error between calculated and reported ratios
-    metrics : dict
-        Dictionary of financial metrics and ratios
+    Args:
+        financial_data (FinancialData): Object containing all financial data.
+        verbose (bool, optional): Whether to print verbose messages during analysis. Defaults to False.
+
+    Attributes:
+        data (FinancialData): Object containing all financial data.
+        verbose (bool): Whether to print verbose messages during analysis.
+        calculated_metrics (pd.DataFrame): Contains all calculated financial metrics and ratios.
+        calculation_error_dict (Dict): Contains the error count and message for each financial metric.
+        fractional_metric_errors (pd.DataFrame): Contains fractional errors for each financial metric.
+
+    Methods:
+        print_metric_errors(metric_errors, tolerance=0.05):
+            Prints the number of values for each metric that exceed the given tolerance level.
+        assert_non_null_frame(df):
+            Asserts that a given dataframe is not null. Raises an AssertionError if the dataframe is null.
+        concat_stock_eval_ratios(df):
+            Concatenates and returns a dataframe containing stock evaluation ratios.
+        concat_profitability_ratios(df):
+            Concatenates and returns a dataframe containing profitability ratios.
+        concat_debt_interest_ratios(df):
+            Concatenates and returns a dataframe containing debt and interest ratios.
+        concat_liquidity_ratios(df):
+            Concatenates and returns a dataframe containing liquidity ratios.
+        concat_efficiency_ratios(df):
+            Concatenates and returns a dataframe containing efficiency ratios.
+        concat_metric_growth(df):
+            Concatenates and returns a dataframe containing the growth rate for each financial metric.
+        analyse() -> pd.DataFrame:
+            Returns a dataframe containing all calculated financial metrics and ratios.
+        cross_check_metric_calculations() -> pd.DataFrame:
+            Cross-checks reported financial metrics against calculated financial metrics and returns 
+            the fractional errors.
+
     """
     def __init__(self, financial_data:FinancialData, verbose: bool=False) -> None:
+        """
+        Initializes the ManualAnalysis object.
+
+        Args:
+            financial_data (FinancialData): Object containing all financial data.
+            verbose (bool, optional): Whether to print verbose messages during analysis. Defaults to False.
+
+        Raises:
+            AssertionError: If any of the calculated metrics are null.
+        """
         self.data = financial_data
         self.verbose = verbose
         self.calculated_metrics = self.analyse()
@@ -459,6 +483,13 @@ class ManualAnalysis:
             self.print_metric_errors(self.fractional_metric_errors, 0.05)
 
     def print_metric_errors(self, metric_errors: pd.DataFrame, tolerance: float=0.05):
+        """
+        Prints the number of values for each metric that exceed the given tolerance level.
+
+        Args:
+            metric_errors (pd.DataFrame): Contains the fractional errors for each financial metric.
+            tolerance (float, optional): The tolerance level for the error fraction. Defaults to 0.05.
+        """
         if not self.verbose:
             return
         line_count = len(metric_errors)
@@ -469,11 +500,41 @@ class ManualAnalysis:
             print(message)
 
     def assert_non_null_frame(self, df: pd.DataFrame):
+        """
+        Raises an AssertionError if any column in the input DataFrame is completely null.
+
+        Args:
+            df (pandas.DataFrame): A pandas DataFrame.
+
+        Raises:
+            AssertionError: If any column in `df` is completely null.
+
+        """
         err_msg = 'Metric error calculations returned a null dataframe. Unreliable calculations.'
         for header in df.columns:
             assert not df[header].isnull().all(), err_msg
                 
     def concat_stock_eval_ratios(self, df: pd.DataFrame):
+        """Calculate and concatenate stock evaluation ratios to the given DataFrame.
+
+        Args:
+            df (pandas.DataFrame): The DataFrame to concatenate the calculated metrics to.
+
+        Returns:
+            pandas.DataFrame: The resulting DataFrame containing the calculated metrics.
+
+        Calculates the following stock evaluation ratios:
+            - Earnings per share (EPS)
+            - Diluted earnings per share (EPSDiluted)
+            - Price to earnings ratio (PE) for high, low, and average closing stock prices
+            - Book value per share
+            - Dividend payout ratio
+            - Dividend yield for low, high, and average closing stock prices
+            - EBITDA ratio
+            - Cash per share
+
+        All ratios are calculated using financial data from the associated FinancialData object.
+        """
         total_assets = self.data.balance_sheets['totalAssets'].copy()
         total_liabilities = self.data.balance_sheets['totalLiabilities'].copy()
         long_term_debt = self.data.balance_sheets['longTermDebt'].copy()
@@ -499,6 +560,26 @@ class ManualAnalysis:
         return df
     
     def concat_profitability_ratios(self, df: pd.DataFrame):
+        """
+        Concatenates profitability ratios to the given DataFrame.
+
+        Args:
+            df (pd.DataFrame): DataFrame to which profitability ratios will be added.
+
+        Returns:
+            pd.DataFrame: DataFrame with added profitability ratios.
+
+        Calculates the following profitability ratios:
+            - Gross profit margin
+            - Operating profit margin
+            - Pretax profit margin
+            - Net profit margin
+            - Return on invested capital (ROIC)
+            - Return on equity (ROE)
+            - Return on Assets (ROA)
+
+        All ratios are calculated using financial data from the associated FinancialData object.
+        """
         revenue = self.data.income_statements['revenue'].copy()
         total_assets = self.data.balance_sheets['totalAssets'].copy()
         gross_profit = self.data.income_statements['grossProfit'].copy()
@@ -517,6 +598,23 @@ class ManualAnalysis:
         return df
     
     def concat_debt_interest_ratios(self, df: pd.DataFrame):
+        """
+        Concatenates debt and interest ratios to the given DataFrame.
+
+        Args:
+            df (pd.DataFrame): DataFrame to which debt and interest ratios will be added.
+
+        Returns:
+            pd.DataFrame: DataFrame with added debt and interest ratios.
+
+        Calculates the following debt and interest ratios:
+            - Interest coverage
+            - Fixed charge coverage
+            - Debt to total capitalization ratio
+            - Total debt ratio
+
+        All ratios are calculated using financial data from the associated FinancialData object.
+        """
         operating_income = self.data.income_statements['operatingIncome'].copy()
         total_assets = self.data.balance_sheets['totalAssets'].copy()
         long_term_debt = self.data.balance_sheets['longTermDebt'].copy()
@@ -534,6 +632,22 @@ class ManualAnalysis:
         return df
 
     def concat_liquidity_ratios(self, df: pd.DataFrame):
+        """
+        Concatenates liquidity ratios to the given DataFrame.
+
+        Args:
+            df (pd.DataFrame): DataFrame to which liquidity ratios will be added.
+
+        Returns:
+            pd.DataFrame: DataFrame with added liquidity ratios.
+
+        Calculates the following liquidity ratios:
+            - Current ratio
+            - Quick ratio
+            - Cash ratio
+
+        All ratios are calculated using financial data from the associated FinancialData object.
+        """
         current_assets = self.data.balance_sheets['totalCurrentAssets'].copy()
         current_liabilities = self.data.balance_sheets['totalCurrentLiabilities'].copy()
         inventory = self.data.balance_sheets['inventory'].copy()
@@ -545,6 +659,26 @@ class ManualAnalysis:
         return df
     
     def concat_efficiency_ratios(self, df: pd.DataFrame):
+        '''
+        Concatenates efficiency ratios to the given DataFrame.
+
+        Args:
+            df (pd.DataFrame): DataFrame to which efficiency ratios will be added.
+
+        Returns:
+            pd.DataFrame: DataFrame with added efficiency ratios.
+
+        Calculates the following efficiency ratios:
+            - Total asset turnover
+            - Inventory to sales ratio
+            - Inventory turnover ratio
+            - Inventory turnover in days
+            - Accounts receivable to sales ratio
+            - Receivables turnover
+            - Receivables turnover in days
+
+        All ratios are calculated using financial data from the associated FinancialData object.
+        '''
         inventory = self.data.balance_sheets['inventory'].copy()
         revenue = self.data.income_statements['revenue'].copy()
         total_assets = self.data.balance_sheets['totalAssets'].copy()
@@ -561,6 +695,29 @@ class ManualAnalysis:
         return df
     
     def concat_metric_growth(self, df: pd.DataFrame):
+        '''
+        Concatenates growth rates for specified metrics to the given DataFrame.
+
+        Args:
+            df (pd.DataFrame): DataFrame to which growth rates will be added.
+
+        Returns:
+            pd.DataFrame: DataFrame with added growth rates.
+
+        Calculates the percentage growth rate for the following metrics over a one year or four quarter period:
+            - Earnings per share (EPS)
+            - Return on equity (ROE)
+            - Cash per share
+            - Price-to-earnings (P/E) ratio (average close)
+            - Earnings before interest, taxes, depreciation, and amortization (EBITDA) ratio
+            - Return on invested capital (ROIC)
+            - Net profit margin
+            - Return on assets (ROA)
+            - Debt to total capitalization ratio
+            - Total debt ratio
+
+        All growth rates are calculated using financial data from the associated FinancialData object.
+        '''
         growth_metrics = ['eps', 'returnOnEquity', 'cashPerShare', 'PE_avg_close', 'ebitdaratio', 'ROIC',
                    'netProfitMargin', 'returnOnAssets', 'debtToTotalCap', 'totalDebtRatio']
         span, init_idx = (1,1) if self.data.period == 'annual' else (4,4)
@@ -577,8 +734,20 @@ class ManualAnalysis:
         return df
     
     def analyse(self) -> pd.DataFrame: 
-        """Calculates and returns important financial metrics and ratios as a 
-            pandas DataFrame."""
+        """
+        Analyzes the financial data and calculates a variety of financial ratios.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the calculated financial ratios.
+
+        The following ratio types are calculated using financial data from the associated FinancialData object:
+            - Stock evaluation ratios
+            - Profitability ratios
+            - Debt and interest ratios
+            - Liquidity ratios
+            - Efficiency ratios
+            - Metric growth
+        """
         df = pd.DataFrame(index=self.data.frame_indecies)
         df = self.concat_stock_eval_ratios(df)
         df = self.concat_profitability_ratios(df)
@@ -589,6 +758,27 @@ class ManualAnalysis:
         return df
 
     def cross_check_metric_calculations(self) -> pd.DataFrame:
+        """
+        Calculates the fractional error between reported metrics and those calculated by the program 
+        for a selection of key metrics. If verbose is set to False, returns None.
+
+        Returns:
+            pd.DataFrame: DataFrame containing the fractional errors for each metric.
+
+        Metrics that are cross-checked:
+            - Gross profit margin
+            - Operating profit margin
+            - Net profit margin
+            - Current ratio
+            - Return on equity (ROE)
+            - Return on assets (ROA)
+            - Cash per share
+            - Interest coverage
+            - Dividend payout ratio
+
+        Fractional error is calculated as the absolute value of the difference between the calculated 
+        metric and the reported metric, divided by the calculated metric.
+        """
         ## add ROE, ROIC, ebitda ratio
         if not self.verbose:
             return
