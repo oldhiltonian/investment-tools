bd610a9d5be1f765521b6846a0babb61f88ac64e
unknown <oldwhpsian@gmail.com>
2023-02-21 13:06:19 +0000

    Added docstring and type hints to the Company class

--- investment_tools/fmp/fmp_layer_1.py
+++ investment_tools/fmp/fmp_layer_1.py
@@ -13,6 +13,7 @@ import pandas as pd
 from pathlib import Path
 import os
 import time
+from typing import Dict, Tuple
 
 yf.pdr_override()
 
@@ -1141,7 +1142,12 @@ class Plots:
 
     def plot_metrics(self):
         '''
-        The plot_metrics method generates plots of financial metrics for a given company. The method first generates the necessary subplots to hold each metric type. Each metric type is then plotted on an individual axis within the metric type's subplot. Each metric's trend data is gathered, and its trend is plotted on the selected axis. A linear trendline is also plotted along with its corresponding R2 value. Finally, each metric type's subplot is given a title and added to the list of plots.
+        The plot_metrics method generates plots of financial metrics for a given company. The method first 
+            generates the necessary subplots to hold each metric type. Each metric type is then plotted on 
+            an individual axis within the metric type's subplot. Each metric's trend data is gathered, and 
+            its trend is plotted on the selected axis. A linear trendline is also plotted along with its 
+            corresponding R2 value. Finally, each metric type's subplot is given a title and added to the 
+            list of plots.
 
         Args:
         self (Plots): The Plots instance.
@@ -1297,7 +1303,8 @@ class Company:
     api_key (str): API key for accessing financial data from a data source
     data (str, optional): Source of data either from 'online' (default) or 'offline'
     period (str, optional): Financial period to use for the analysis either 'annual' (default) or 'quarterly'
-    limit (int, optional): The number of financial periods to include in the analysis (default is 120)
+    limit (int, optional): The number of financial periods to include in the analysis (default is 20)
+    verbose (bool, optional): If True, verbose output will be printed during analysis (default is False)
 
     Attributes:
     ticker (str): The stock symbol representing the company
@@ -1305,8 +1312,116 @@ class Company:
     metrics (dict): Dictionary of financial metrics for the company
     trends (list of plot objects): List of plots showing the trend of the financial metrics over time
 
+    Methods:
+    __init__(self, ticker, api_key, data='online', period='annual', limit=20, verbose=False):
+        Initializes a new instance of the `Company` class.
+
+        Parameters:
+        ticker (str): The stock symbol representing the company
+        api_key (str): API key for accessing financial data from a data source
+        data (str, optional): Source of data either from 'online' (default) or 'offline'
+        period (str, optional): Financial period to use for the analysis either 'annual' (default) or 'quarterly'
+        limit (int, optional): The number of financial periods to include in the analysis (default is 20)
+        verbose (bool, optional): If True, verbose output will be printed during analysis (default is False)
+
+    recommendation(self) -> Dict[str, dict]:
+        Builds a recommendation for the company based on its financial metrics.
+
+        Returns:
+        A dictionary with the keys being the metrics and the values being another dictionary with 'score'
+             and 'strength' keys.
+
+    get_modifier(self, metric: str) -> int:
+        Returns a modifier based on whether the metric is related to debt.
+
+        Parameters:
+        metric (str): The name of the financial metric.
+
+        Returns:
+        1 if the metric is not related to debt, -1 otherwise.
+
+    get_copy_of_df_column(self, header: str) -> pd.Series:
+        Returns a copy of a column of data for a given metric.
+
+        Parameters:
+        header (str): The name of the financial metric.
+
+        Returns:
+        A copy of the column of data for the given metric.
+
+    get_r2_val(metrics: pd.Series) -> float:
+        Returns the R-squared value for a given series of metrics.
+
+        Parameters:
+        metrics (pd.Series): A series of metrics.
+
+        Returns:
+        The R-squared value for the given series of metrics.
+
+    score(self, metric: str) -> Tuple[int, int]:
+        Computes the score for a given metric.
+
+        Parameters:
+        metric (str): The name of the financial metric.
+
+        Returns:
+        A tuple containing the growth score and the stability score for the given metric.
+
+    score_mean_growth(self, mean_growth: float) -> int:
+        Computes the growth score for a given mean growth rate.
+
+        Parameters:
+        mean_growth (float): The mean growth rate.
+
+        Returns:
+        The growth score for the given mean growth rate.
+
+    score_trend_strength(r2: float) -> int:
+        Calculates the trend strength score based on the R^2 value of a metric.
+    
+        Args:
+        r2 (float): The R^2 value of a metric.
+        
+        Returns:
+        int: The trend strength score.
+
+    eval_(scores: Dict[str, dict]) -> bool:
+        Evaluates the overall recommendation for the company based on the scores for each metric.
+        
+        Args:
+        scores (Dict[str, dict]): A dictionary of scores for each metric.
+        
+        Returns:
+        bool: True if the recommendation is positive, False otherwise.
+
+    print_charts() -> None:
+        Prints the trend charts for each metric.
+        
+        Returns:
+        None
+
+    export() -> None:
+        Exports the financial trend charts to disk as a pdf file. Also exports key findings based on the analysis.
     """
+
     def __init__(self, ticker, api_key, data='online', period='annual', limit=20, verbose=False):
+        """
+        Initialize a new instance of the Company class.
+
+        Args:
+            ticker (str): The stock symbol representing the company.
+            api_key (str): API key for accessing financial data from a data source.
+            data (str, optional): Source of data either from 'online' (default) or 'offline'.
+            period (str, optional): Financial period to use for the analysis either 'annual' (default) or 'quarterly'.
+            limit (int, optional): The number of financial periods to include in the analysis (default is 20).
+            verbose (bool, optional): Set to True to enable additional console output (default is False).
+
+        Attributes:
+            ticker (str): The stock symbol representing the company.
+            period (str): Financial period used for the analysis.
+            metrics (dict): Dictionary of financial metrics for the company.
+            trends (list of plot objects): List of plots showing the trend of the financial metrics over time.
+        """
         self.ticker = ticker
         self.period = period
         self.limit = limit
@@ -1327,12 +1442,14 @@ class Company:
             self.export()
         
 
-    def recommendation(self):
-        '''Built to look at the main metrics that indicate value to shareholders. Debt is also included but is handled differently
-            than the equity metrics in the self.score() function
-            Why these metrics: The equity ones are the main metrics regarding value to shareholders
-            Debt is also important, as a company that is paying down debt is putting itself in a good position
-            Tradeoff tough, as if there is no debt then there will be no decrease in debt, which will score badly.'''
+    def recommendation(self) -> Dict[str, dict]:
+        """
+        Compute a recommendation for the company based on key financial metrics.
+
+        Returns:
+        scores (Dict[str, dict]): A dictionary containing the scores and strengths for each 
+        financial metric considered.
+        """
         key_metrics = ['eps_growth', 'returnOnEquity_growth', 'ROIC_growth', 'returnOnAssets_growth', 
                        'debtToTotalCap_growth', 'totalDebtRatio_growth']
         scores = dict()
@@ -1341,77 +1458,163 @@ class Company:
             scores[metric] = {'score': score,
                               'strength': strength}
         return scores
-        
 
-    def score(self, metric):
+    def get_modifier(self, metric: str) -> int:
+        """
+        Returns a modifier based on the provided metric. If the metric name contains
+        'debt', the modifier is -1. Otherwise, it is 1.
+
+        Args:
+        metric (str): The name of the metric.
+
+        Returns:
+        int: The modifier to be used in calculations.
+        """
         if 'debt' in metric.lower():
-            modifier = -1
+            return -1
         else:
-            modifier = 1
-        
-        metrics = self.metrics[metric].copy()
-        metrics = metrics.dropna()
-        mean = metrics.mean()
+            return 1
+
+    def get_copy_of_df_column(self, header: str) -> pd.Series:
+        """
+        Returns a copy of a pandas Series for a given column header in the calculated metrics DataFrame.
+
+        Args:
+        header (str): The header of the column to return.
+
+        Returns:
+        pd.Series: A copy of the column as a pandas Series with any missing values removed.
+        """
+        return self.metrics[header].copy().dropna()
+
+    def get_r2_val(self, metrics: pd.Series) -> float:
+        """
+        Calculates and returns the R-squared value of a linear regression model for the given Pandas series of metrics.
+
+        Args:
+        metrics (pd.Series): A Pandas series containing the metric values.
+
+        Returns:
+        float: The R-squared value of the linear regression model.
+
+        """
         try:
             slope, intercept, r_val, _, _= linregress(range(len(metrics)), metrics)
-            r_val = r_val**2
+            return r_val**2
         except ValueError:
-            r_val = 0
+            return 0.
 
+    def score(self, metric: str) -> Tuple[int, int]:
+        """
+        Calculates the growth score and the trend stability score of the given financial metric.
+        
+        Args:
+        metric (str): The name of the financial metric to score.
+        
+        Returns:
+        A tuple containing the growth score and the trend stability score.
+        The growth score is an integer value between 0 and 4, indicating the mean growth rate of the metric.
+        The trend stability score is an integer value between 0 and 4, indicating the strength of the trend of the metric.
+        """
+        modifier = self.get_modifier(metric)
+        metrics = self.get_copy_of_df_column(metric)
+        mean = metrics.mean()
+        r2 = self.get_r2_val(metrics)
         growth_score = self.score_mean_growth(modifier * mean)
-        stability_score = self.score_trend_strength(r_val)
-
+        stability_score = self.score_trend_strength(r2)
         return (growth_score, stability_score)
 
 
-    def score_mean_growth(self, mean_growth):
+    def score_mean_growth(self, mean_growth: float) -> int:
+        """
+        Scores the mean growth of a metric based on the following ranges:
+
+        - 0: <= 0.05
+        - 1: <= 0.10
+        - 2: <= 0.15
+        - 3: <= 0.20
+        - 4: > 0.20
+
+        Args:
+            mean_growth (float): The mean growth of a financial metric
+
+        Returns:
+            int: The score for the mean growth of the metric based on the above ranges.
+        """
         growth = float(mean_growth)
         if growth <= 0.05:
-            indicator = 0
+            return 0
         elif growth <= 0.10:
-            indicator = 1
+            return 1
         elif growth <= 0.15:
-            indicator = 2
+            return 2
         elif growth <= 0.2:
-            indicator = 3
+            return 3
         else:
-            indicator = 4
-
-        return indicator
+            return 4
     
-    def score_trend_strength(self, R2):
-        R2_ = float(R2)
-        if R2_ <= 0.2:
-            strength = 0
-        elif R2_ <= 0.3:
-            strength = 1
-        elif R2_ <= 0.5:
-            strength = 2
-        elif R2_  <= 0.75:
-            strength = 3
-        else:
-            strength = 4
-        
-        return strength
+    def score_trend_strength(self, r2: float) -> int:
+        """
+        Calculate the trend strength score for a financial metric.
 
-    def eval_(self, scores):
+        Args:
+            r2 (float): The coefficient of determination (R^2) of the trend line.
+
+        Returns:
+            int: The trend strength score for the metric, from 0 to 4.
+
+        """
+        r2_ = float(r2)
+        if r2_ <= 0.2:
+            return 0
+        elif r2_ <= 0.3:
+            return 1
+        elif r2_ <= 0.5:
+            return 2
+        elif r2_  <= 0.75:
+            return 3
+        else:
+            return 4
+        
+
+    def eval_(self, scores: Dict[str, dict]) -> bool:
+        """
+        Determines if the company has favorable financial metrics based on the provided scores.
+
+        Args:
+        scores (Dict[str, dict]): A dictionary of scores and strengths for various financial metrics
+
+        Returns:
+        bool: True if the company has favorable financial metrics based on the provided scores, False otherwise.
+        
+        """
         vote = 0
         for key in scores.keys():
             vote += scores[key]['score']
         # Require an average score of 1.5 for each metric
         return False if vote < 1.5*len(scores) else True 
 
-    def print_charts(self):
+    def print_charts(self) -> None:
+        """
+        Prints the trend charts for the company's financial metrics.
+        
+        If the charts have already been printed, this method does nothing.
+        
+        Returns:
+        None
+        """
         if self._charts_printed:
             return
         self._plots = Plots(self.ticker, self.period, self.metrics, self.limit, self.filing_dates)
         self.trends = self._plots.plots
         self._charts_printed = True
     
-    def export(self):
+    def export(self) -> None:
         """
-        Exports the financial trend charts to disk as a pdf file.
-        
+        The export() method exports the financial trend charts to a PDF file.
+
+        Returns:
+        None.
         """
         self._plots._export_charts_pdf()
         # also export key findings based on the analyis
